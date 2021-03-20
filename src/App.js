2a7ac3856cc393fa7259d3207ea6832b979a6b92
austenroyer <60246799+austenroyer@users.noreply.github.com>
2021-03-20 18:40:17 -0400

    Top and bottom panels in left side of screen

--- src/App.js
+++ src/App.js
@@ -2,40 +2,76 @@ import logo from './logo.svg';
 import './App.css';
 import Map from './Map'
 const styles = {
-  flexbox:
-    {
+  flexbox: {
       display: 'grid',
       direction: 'row',
       gridColumnGap: '1vw',
       gridRowGap: '1vh',
       height:'100%'
-
     },
     item1: {
-      gridColumnStart: 1,
-      gridColumnEnd: 29,
+      gridColumnStart: 2,
+      gridColumnEnd: 31,
       gridRowStart: 10,
-      gridRowEnd: 90,
-      border: '1px solid',
-      borderRadius: '25%',
+      gridRowEnd: 80,
+      border: '5px solid',
+      backgroundColor: '#ffff00',
+      textAlign: 'center',
+      display: 'grid',
+      direction: 'column',
+      gridColumnGap: '1vw',
+      gridRowGap: '1vh',
+      height:'100%'
+      // borderRadius: '25%',
+    },
+    topHalf: {
+      gridColumnStart: 2,
+      gridColumnEnd: 31,
+      gridRowStart: 10,
+      gridRowEnd: 30,
+      border: '5px solid',
+      backgroundColor: '#0000ff',
+    },
+    bottomHalf: {
+      gridColumnStart: 2,
+      gridColumnEnd: 31,
+      gridRowStart: 40,
+      gridRowEnd: 60,
+      border: '5px solid',
+      backgroundColor: '#00ff00',
     },
     item2: {
-      gridColumnStart: 30,
+      gridColumnStart: 33,
       gridColumnEnd: 95,
       gridRowStart: 1,
       gridRowEnd: 90,
-          }
+    },
+    title: {
+        backgroundColor: '#282c34',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 'calc(10px + 2vmin)',
+        color: 'white',
+        // marginTop: '3vh',
+        // marginBottom:'3vh',
+      }
 }
 function App() {
   return (
-    <div>
+    <div style = {{backgroundColor: '#dddddd'}}>
       <p className={"App-header"}> Police Butt Map</p>
     <div style={styles.flexbox} >
       <div style = {styles.item1}>
-        <p> First Item!</p>
+        <p> Hello there, here is a tutorial for how to use this tool peepeepoopoo</p>
+        <div styles={styles.bottomHalf}>
+          <p> THere!</p>
+        </div>
       </div>
       <div style={styles.item2}>
-        <Map />
+        <Map style = {{      border: '5px solid #0099ff',
+      borderRadius: '1%',}}/>
       </div>
     </div>
     </div>
