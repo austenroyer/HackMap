8a6b652bbcf3761a13d56392d7ef5bfcd5e13b76
austenroyer <60246799+austenroyer@users.noreply.github.com>
2021-03-20 17:25:09 -0400

    Vertical split in page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import Map from './Map'
+const styles = {
+  flexbox:
+    {
+      display: 'grid',
+      direction: 'row',
+      gridColumnGap: '1vw',
+      gridRowGap: '1vh',
+      height:'100%'
 
+    },
+    item1: {
+      gridColumnStart: 20,
+      gridColumnEnd: 30,
+      gridRowStart: 40,
+      gridRowEnd: 60,
+    },
+    item2: {
+      gridColumnStart: 40,
+      gridColumnEnd: 95,
+      gridRowStart: 12,
+      gridRowEnd: 87,
+          }
+}
 function App() {
   return (
-    <div className="App-header" >
-      <Map style={{maxWidth: '50%', width: '50%', height: '75%', maxHeight:'75%'}}/>
+    <div style={styles.flexbox} >
+      <div style = {styles.item1}>
+        <p> First Item!</p>
+      </div>
+      <div style={styles.item2}>
+        <Map style={{maxWidth: '50%', width: '50%', height: '75%', maxHeight:'75%'}}/>
+      </div>
     </div>
   );
 }
