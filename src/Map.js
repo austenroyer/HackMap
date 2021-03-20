51e8ad86bdf2f54a480886d47c2526b0a78b894c
Hiro Kuwana <hkuwana97@gmail.com>
2021-03-20 19:11:52 -0400

    Hello

--- src/Map.js
+++ src/Map.js
@@ -3,19 +3,8 @@ import React, { Component } from 'react';
  
 export class MapContainer extends Component{
   render() {
-    var points = [
-      { lat: 42.02, lng: -77.01 },
-      { lat: 42.03, lng: -77.02 },
-      { lat: 41.03, lng: -77.04 },
-      { lat: 42.05, lng: -77.02 }
-        ];
-        var bounds = new this.props.google.maps.LatLngBounds();
-        for (var i = 0; i < points.length; i++) {
-          bounds.extend(points[i]);
-        }
-        
     return (
-      <Map containerStyle={{position: 'absolute', width: '65vw', height: '85vh'}} style = {this.props.style} google={this.props.google} zoom={4} initialCenter={{lat: 38.7, lng: -96.4}} 
+      <Map containerStyle={{position: 'absolute', width: '65vw', height: '85vh'}} style = {this.props.style} google={this.props.google} zoom={10} initialCenter={{lat: 41.82, lng: -71.41}} 
       >
  
         {/* <Marker onClick={this.onMarkerClick}
