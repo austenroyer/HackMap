045dbd65bda66141d1d65b5cbf9bf9c30f26b46d
austenroyer <60246799+austenroyer@users.noreply.github.com>
2021-03-20 17:10:25 -0400

    Zoom and coords

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Map from './Map'
 
 function App() {
   return (
-    <div className="App">
-      <Map/>
+    <div className="App-header" >
+      <Map style={{maxWidth: '50%', width: '50%', height: '75%', maxHeight:'75%'}}/>
     </div>
   );
 }
--- src/Map.js
+++ src/Map.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
  
 export class MapContainer extends Component{
   render() {
+    var points = [
+      { lat: 42.02, lng: -77.01 },
+      { lat: 42.03, lng: -77.02 },
+      { lat: 41.03, lng: -77.04 },
+      { lat: 42.05, lng: -77.02 }
+        ];
+        var bounds = new this.props.google.maps.LatLngBounds();
+        for (var i = 0; i < points.length; i++) {
+          bounds.extend(points[i]);
+        }
+        
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map style = {this.props.style} google={this.props.google} zoom={3.7} initialCenter={{lat: 38.7, lng: -96.4}} 
+      >
  
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+        {/* <Marker onClick={this.onMarkerClick}
+                name={'Current location'} /> */}
  
         <InfoWindow onClose={this.onInfoWindowClose}>
             <div>
