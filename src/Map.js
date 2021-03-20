8526cf1b60f4dbb6777a216ff7e7757a45511fff
Menoikeus <datonnguyen@gmail.com>
2021-03-20 16:36:55 -0400

    Map working

---
+++ src/Map.js
@@ -0,0 +1,24 @@
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from 'react';
+ 
+export class MapContainer extends Component{
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>HIII</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+ 
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyACIqLOutzdazZcxxa0pyUffXSZLqQpyPo")
+})(MapContainer)
